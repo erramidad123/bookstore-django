86dddc0853c3db29dfb507ea6d8f9c3745fcf881
rachiderrami <errami.rachid@outlook.com>
2023-01-30 21:30:49 -0500

    adding procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn book_project.wsgi --log-file -
+web: gunicorn -b 0.0.0.0:8000 backend.django_app.mainapp:application --log-file -
--- book_project/settings.py
+++ book_project/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -128,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 EMAIL_USE_TLS = True 
 
 EMAIL_HOST_USER = '*******'
